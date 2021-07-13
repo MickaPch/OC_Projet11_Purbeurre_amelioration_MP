56063311d1487f49a24172c7295799ab0a3dce3f
MickaP <mickael.perchet@gmail.com>
2021-07-13 10:23:38 +0200

    IT Consulting - Correction bug newsletter

--- user/views.py
+++ user/views.py
@@ -119,14 +119,13 @@ class NewAccountView(TemplateView):
                     user_login = email
                 firstname = form_new.cleaned_data["firstname"]
                 lastname = form_new.cleaned_data["lastname"]
-                newsletter = form_new.cleaned_data["newsletter"]
+                # newsletter = form_new.cleaned_data["newsletter"]
                 user = User.objects.create_user(
                     email,
                     password=pwd,
                     username=user_login,
                     first_name=firstname,
-                    last_name=lastname,
-                    newsletter=newsletter
+                    last_name=lastname
                 )
                 user.save()
                 data['ok'] = True
