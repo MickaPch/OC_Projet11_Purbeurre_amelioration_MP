5356881bec0f3091475b3936da441e29bc04713b
MickaP <mickael.perchet@gmail.com>
2021-08-13 15:58:53 +0200

    change mail settings

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -196,13 +196,13 @@ STATICFILES_DIRS = [
 
 # Production email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'elfe.o2switch.net'
+EMAIL_HOST = 'mail.mickapr.fr'
+EMAIL_USE_TLS = False
+EMAIL_PORT = 465
 EMAIL_HOST_USER = 'do-not-reply@purbeurre.mickapr.fr'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = True
-EMAIL_PORT = 465
+# EMAIL_USE_SSL = True
 
 INTERNAL_IPS = ['127.0.0.1',]
 
